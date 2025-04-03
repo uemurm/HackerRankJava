8e33c04ef839f0ffb2eb6a098276bc82ef6c7faf
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2025-04-03 11:01:21 +1100

    Correct package name.

---
+++ src/main/java/com/rock/yacht/A03_InheritAndOverride.java
@@ -0,0 +1,32 @@
+package com.rock.yacht;
+
+class Cycle {
+    private final String name;
+
+    public Cycle() {
+        name = "a cycle";
+    }
+
+    String getName() {
+        return name;
+    }
+}
+
+class MotorCycle extends Cycle {
+    @Override
+    String getName() {
+        return "a motorcycle";
+    }
+
+    void sayHello() {
+        System.out.println("Hello I am " + getName() + ", I am " + super.getName() + " with an engine.");
+        System.out.println("My ancestor is " + super.getName() + " who is a vehicle with pedals.");
+    }
+}
+
+public class A03_InheritAndOverride {
+    public static void main(String[] args) {
+        MotorCycle mc = new MotorCycle();
+        mc.sayHello();
+    }
+}
