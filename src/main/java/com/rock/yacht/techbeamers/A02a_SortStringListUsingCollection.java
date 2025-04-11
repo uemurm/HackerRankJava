546c0725f585531e2488def757171f89aa3632fb
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2025-04-11 14:36:28 +1000

    Implement A02 using Collections.

---
+++ src/main/java/com/rock/yacht/techbeamers/A02a_SortStringListUsingCollection.java
@@ -0,0 +1,35 @@
+package com.rock.yacht.techbeamers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+// Write code to sort the list of strings without using Java collection.
+public class A02a_SortStringListUsingCollection {
+    public static void main(String[] args) {
+        List<String> inputList = new ArrayList<>(Arrays.asList(
+                // Note some are in lowercase.
+                "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "aug", "Sep", "Oct", "nov", "Dec"
+        ));
+
+        System.out.println("---- Input List ----");
+        showList(inputList);
+
+        System.out.println("---- Sorted List in case-sensitive ----");
+        inputList.sort(String::compareTo);
+        showList(inputList);
+
+        System.out.println("---- Sorted List in case-insensitive ----");
+        inputList.sort(String::compareToIgnoreCase);
+        showList(inputList);
+    }
+
+    public static void showList(List<String> array) {
+        for (String str: array) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+    }
+}
